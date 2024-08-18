1fe7da870fa9de9118ec8c86ebf628a25b2fb087
kayquesanmartin <kayque.sanmartin65@gmail.com>
2024-08-18 17:00:41 -0300

    feat: Criação do projeto

---
+++ src/main/java/tech/kayque/gerenciadordetarefas/GerenciadordetarefasApplication.java
@@ -0,0 +1,13 @@
+package tech.kayque.gerenciadordetarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadordetarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadordetarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/kayque/gerenciadordetarefas/GerenciadordetarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.kayque.gerenciadordetarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadordetarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
